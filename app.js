0a9922c36377b6f89e91c8413dfd7096877049db
mlucas4330 <mlucas4330@gmail.com>
2024-12-11 13:23:32 -0300

    nginx docker-compose update

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ import { env } from './src/env.js'
 import router from './src/router.js'
 import cookieParser from 'cookie-parser'
 import { database } from './src/datatabase.js'
+import { readFileSync } from 'fs'
+import { createServer } from 'https'
 
 const main = async () => {
     try {
@@ -20,9 +22,22 @@ const main = async () => {
         app.use(cookieParser())
         app.use(router)
 
-        app.listen(env.port, () => {
-            console.log(`Server running on: ${env.base_url}`)
-        })
+        if(env.node_env === 'production'){
+            const options = {
+                key: readFileSync('/etc/letsencrypt/live/api.trendtext.com.br/privkey.pem'),
+                cert: readFileSync('/etc/letsencrypt/live/api.trendtext.com.br/fullchain.pem')
+            };
+    
+            const https = createServer(options, app);
+    
+            https.listen(env.port, () => {
+                console.log(`Server running on: ${env.base_url}`);
+            });
+        } else {
+            app.listen(env.port, () => {
+                console.log(`Server running on: ${env.base_url}`)
+            })
+        }
     } catch (err) {
         console.error(err.message)
     }
